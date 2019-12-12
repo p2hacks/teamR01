29d0d33d5f9d62d37083e84986094251b5a44d14
Misodat85 <b1018225@fun.ac.jp>
2019-12-13 05:58:26 +0900

    first test

--- server/send-message/main.go
+++ server/send-message/main.go
@@ -1,21 +1,30 @@
 package main
 
-import (
+import(
+	"github.com/jinzhu/gorm"
 	"github.com/labstack/echo"
-	github.com/labstack/echo/middleware"
+	"github.com/labstack/echo/middleware"
+	"github.com/p2hacks/teamR01/server/send-message/controller"
+	"github.com/p2hacks/teamR01/server/send-message/model"
+	"github.com/p2hacks/teamR01/server/send-message/config"
+	"github.com/p2hacks/teamR01/server/send-message/dbconnect"
 )
 
-func main() {
+func main(){
+	var ctrl controller.IsController
 	//creating new instance
 	e := echo.New()
 
 	e.Use(middleware.Recover())
 	e.Use(middleware.Logger())
 	//TODO データベースの初期化
-
+	db, err :=connect.InitDB()
+	if err != nil{
+	}
+	ctrl = controller.InitController(db)
 	//TODO ルーティング
-	e.GET("/")
-	e.POST("/send")
+	//e.GET("/",hogehoge)//bff用
+	e.POST("/send",ctrl.controller.MessageHandler)//クライアントからuser_id,messageを受け取るルータ
 	//TODO サーバーを立ち上げる
 	e.Logger.Fatal(e.Start(":8080"))
 }
