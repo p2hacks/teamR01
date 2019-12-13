5eb4072d168b05e0ec50203095d7f0c4a4b6577e
SaKu2110 <b1018106@fun.ac.jp>
2019-12-13 13:35:59 +0900

    update bff api

--- server/bff/bff-api.js
+++ server/bff/bff-api.js
@@ -5,6 +5,19 @@ const { RESTDataSource } = require('apollo-datasource-rest');
 const { ApolloServer } = require('apollo-server');
 
 // 各APIに対するリクエスト処理を記述する
+class UserAPI extends RESTDataSource {
+  constructor() {
+    super();
+    this.baseURL = process.env.USER;
+  }
+
+  async setUser(phoneNum) {
+    return this.post(
+        `set`,
+        {phone: phoneNum},
+      );
+  }
+}
 
 // graphQLの初期化
 const server = new ApolloServer({
@@ -13,6 +26,7 @@ const server = new ApolloServer({
   dataSources: () => {
     return {
         // MicroServiceに対するリクエスト処理のインスタンスを追加
+        UserAPI: new UserAPI(),
     };
   },
 });
--- server/bff/schema/schema.js
+++ server/bff/schema/schema.js
@@ -2,6 +2,11 @@ const { gql } = require('apollo-server');
 
 // クエリを追加する
 module.exports = gql`
+  type User {
+    STATUS:  Boolean
+    ID: String
+  }
   type Query {
+    setUser(phone: String!): User
   }
 `;
