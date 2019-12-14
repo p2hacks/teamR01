d0037b220f1d09c0deeffbace28ac2ec5bdbce52
SaKu2110 <b1018106@fun.ac.jp>
2019-12-14 13:23:52 +0900

    update bff api

--- server/bff/resolver/resolver.js
+++ server/bff/resolver/resolver.js
@@ -7,5 +7,8 @@ module.exports = {
         setRate: async (_source, { id, rate },{ dataSources }) => {
             return dataSources.RateAPI.setRate(id, rate);
         },
+        setPresent: async (_source, { id, asin },{ dataSources }) => {
+            return dataSources.PresentAPI.setPresent(id, asin);
+        },
     },
 };
