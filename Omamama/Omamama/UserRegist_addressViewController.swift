1edb73574cc741fc48819114e7276602930dff27
nao <b1018080@fun.ac.jp>
2019-12-15 10:42:31 +0900

    noma write README

---
+++ Omamama/Omamama/UserRegist_addressViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UserRegist_addressViewController.swift
+//  Omamama
+//
+//  Created by Naoki Noma on 2019/12/15.
+//  Copyright © 2019 shotaro-yamada. All rights reserved.
+//
+
+import UIKit
+
+class UserRegist_addressViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
